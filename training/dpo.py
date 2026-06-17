4610bb0882080af4d6ef0f4d0f2165bb64ebdaf3
Nick Meyer <njm2179@columbia.edu>
2026-06-17 13:37:05 -0400

    small bug on eval mode/no gradients

--- training/dpo.py
+++ training/dpo.py
@@ -19,7 +19,8 @@ dataset = dataloader.get_data()
 # load reference model and tokenizer
 reference_model, tokenizer = load_model(config, checkpoint_path=
                                         config["model"]["checkpoint_path"])
-reference_model.eval()      # set to eval mode to explicit freeze
+reference_model.eval()                  # set to eval mode to disable dropout
+reference_model.requires_grad_(False)   # explicit freeze / stop gradient flow
 
 # load another instantiation of SFT model as the base for DPO training
 base_model, _ = load_model(config, checkpoint_path=
